f118785a920a4f8ecc27d178d98bb16ea993ea2d
22Frankfert <thamamrat.dujmayoon@gmail.com>
2023-09-20 23:00:31 +0700

    feat: change bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
-  int i;
+{ char Name[40], u[5], answer[10];
+  int o;
   do
   {
 //    printf("What is your Name? ");
